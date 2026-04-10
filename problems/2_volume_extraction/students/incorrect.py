fbc333bceb551bfe4ad1de4d76c30f486c304495
Hairum Qureshi <159098695+Hairum-Qureshi@users.noreply.github.com>
2026-04-09 23:13:25 -0400

    added more assert_equal() function tests

--- problems/2_volume_extraction/students/incorrect.py
+++ problems/2_volume_extraction/students/incorrect.py
@@ -1,12 +1,17 @@
 from bakery import assert_equal
 
-PI = 3.1 
+PI = 3.14 
 
 def calculate_area(radius: float) -> float:
-    return PI * radius 
+    return 3.14 * (radius ** 2)
 
 def calculate_volume(radius: float, height: float) -> float:
-    return 3.1 * radius * height
+    return 3.14 * radius * height
 
-assert_equal(calculate_area(1.), 3.14159)
-assert_equal(calculate_volume(1., 2.), 6.28318)
+assert_equal(calculate_area(1.0), 3.14159)
+assert_equal(calculate_area(2.0), 12.56636)
+assert_equal(calculate_area(0.0), 0.0)
+assert_equal(calculate_volume(1.0, 1.0), 3.14159)
+assert_equal(calculate_volume(1.0, 2.0), 6.28318)
+assert_equal(calculate_volume(2.0, 1.0), 12.56636)
+assert_equal(calculate_volume(0.0, 5.0), 0.0)
